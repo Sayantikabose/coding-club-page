f56cf90173a816fdaeac498b263f293efbd1f6b5
SAYANTIKA BOSE <93571416+Sayantikabose@users.noreply.github.com>
2022-02-18 18:43:17 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+window.addEventListener('scroll',
+    moveScrollIndicator);
+
+const scrollIndicatorElt =
+    document.getElementById('scrollIndicator');
+
+const maxHeight =
+    window.document.body.scrollHeight -
+    window.innerHeight;
+
+function moveScrollIndicator(e) {
+
+    const percentage =
+        ((window.scrollY) / maxHeight) * 100;
+    scrollIndicatorElt.style.width = percentage + '%';
+}
